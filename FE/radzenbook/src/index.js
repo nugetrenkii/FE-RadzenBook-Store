c7541f272c7e8ba2e4bff1ced303c5f86e03ee1d
DESKTOP-OOU6OR8\NugetRenki <taikhoancuaban203@gmail.com>
2024-04-23 09:45:21 +0700

    done route

--- FE/radzenbook/src/index.js
+++ FE/radzenbook/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import RouterCustom from './router'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<h1>Radzen Book</h1>
+    <BrowserRouter>
+    <RouterCustom/>
+    </BrowserRouter>
 );
 
---
+++ FE/radzenbook/src/pages/user/Profile/index.js
@@ -0,0 +1,9 @@
+import { memo } from 'react'
+
+const Profile = () => {
+    return (
+        <h1>Profile Screen</h1>
+    );
+}
+
+export default memo(Profile);
