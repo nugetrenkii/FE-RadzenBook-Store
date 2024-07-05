5d5399f211f0ecf6946dc8ab9c97b42f9a01ca1f
kienpt <taikhoancuaban203@gmail.com>
2024-07-05 11:13:40 +0700

    add userContext and fix login

--- FE/radzenbook/src/pages/router.js
+++ FE/radzenbook/src/pages/router.js
@@ -1,4 +1,5 @@
-import { Routes, Route, Navigate  } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { ROUTERS } from "utils/router";
 import {
   HomePageUS,
@@ -18,8 +19,19 @@ import {
 import MasterUsLayout from "./users/theme/masterUsLayout";
 import { CartProvider } from "../pages/users/shoppingCartPage/CartContext";
 import Login_signup from "./login_signup";
+import { UserContext } from "../context/UserContext";
 
-const renderUserRouter = () => {
+const RouterCustom = () => {
+  const { user, loginContext } = useContext(UserContext);
+
+  useEffect (() =>{
+    if(localStorage.getItem("token")){
+      loginContext(localStorage.getItem("email"), localStorage.getItem("token"))
+    }
+  }, [])
+
+  console.log('check user>>>>>>', user);
+
   const userRouters = [
     {
       path: ROUTERS.USER.HOME,
@@ -82,37 +94,35 @@ const renderUserRouter = () => {
       component: <Login_signup />,
     },
   ];
+
   return (
     <CartProvider>
-    <Routes>
-      {loginRouters.map((item, key) => (
+      <Routes>
+        {loginRouters.map((item, key) => (
           <Route key={key} path={item.path} element={item.component} />
         ))}
-      {userRouters.map((item, key) => (
-        <Route
-          key={key}
-          path={item.path}
-          element={<MasterUsLayout>{item.component}</MasterUsLayout>}
-        />
-      ))}
-      {adminRouters.map((item, key) =>
-        item.path.startsWith("/admin") ? (
-          <Route key={key} path={item.path} element={item.component} />
-        ) : (
+        {userRouters.map((item, key) => (
           <Route
             key={key}
             path={item.path}
-            element={<HomePageAdmin screen={item.component} />}
+            element={<MasterUsLayout>{item.component}</MasterUsLayout>}
           />
-        )
-      )}
-      <Route path="/login" element={<Navigate to={ROUTERS.LOGIN.SIGNIN} />} />
-    </Routes>
+        ))}
+        {adminRouters.map((item, key) =>
+          item.path.startsWith("/admin") ? (
+            <Route key={key} path={item.path} element={item.component} />
+          ) : (
+            <Route
+              key={key}
+              path={item.path}
+              element={<HomePageAdmin screen={item.component} />}
+            />
+          )
+        )}
+        <Route path="/login" element={<Navigate to={ROUTERS.LOGIN.SIGNIN} />} />
+      </Routes>
     </CartProvider>
   );
 };
 
-const RouterCustom = () => {
-  return renderUserRouter();
-};
 export default RouterCustom;
--- FE/radzenbook/src/pages/users/theme/header/index.js
+++ FE/radzenbook/src/pages/users/theme/header/index.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from "react";
+import { memo, useEffect, useState, useContext } from "react";
 import {
   AiOutlineDownCircle,
   AiOutlineFacebook,
@@ -20,7 +20,8 @@ import { ROUTERS } from "utils/router";
 import "./style.scss";
 import { categories } from "utils/common";
 import { useCart } from "pages/users/shoppingCartPage/CartContext";
-import { toast,ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
+import { UserContext } from "../../../../context/UserContext";
 
 const HeaderUS = () => {
   const location = useLocation();
@@ -34,8 +35,12 @@ const HeaderUS = () => {
   const [selectedDropdown, setSelectedDropdown] = useState(null);
   const [notifications] = useState(3);
 
+  const { logout, user } = useContext(UserContext);
+
+  console.log('checkkk usserrr>>>>>>>>>>>', user);
+
   const handleLogout = () => {
-    localStorage.removeItem("token");
+    logout();
     navigate("/");
     toast.success('Đăng xuất thành công!')
   }
@@ -144,12 +149,11 @@ const HeaderUS = () => {
         </div>
         <div className="humberger__menu__widget">
           <div className="header__top__right__auth">
-            <Link to="/login">
-              <BiUser /> Đăng nhập
-            </Link>
-            <Link to="/logout">
+            {user && user.auth === true ? <Link to="/logout">
               <BiUser /> Đăng xuất
-            </Link>
+            </Link> : <Link to="/login">
+              <BiUser /> Đăng nhập
+            </Link>}
           </div>
         </div>
         <nav className="humberger__menu__nav">
@@ -216,16 +220,16 @@ const HeaderUS = () => {
 
       <header className="header">
         <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
         />
         {/* Humberger Begin */}
         <div className="header__top">
@@ -245,6 +249,7 @@ const HeaderUS = () => {
                 <div className="header__top__right">
                   <div className="header__top__right__social">
                     <ul>
+                      {user && user.email && <span style={{ marginRight: 20, marginTop: -5 }}>Xin chào: {user.email}</span>}
                       <li>
                         <Link to="">
                           <AiOutlineFacebook />
@@ -266,13 +271,14 @@ const HeaderUS = () => {
                         </Link>
                       </li>
                       <li>
-                        <Link to="/login">
-                          <BiUser />
-                          <span>Đăng nhập</span>
-                        </Link>
-                        <Link onClick={() => handleLogout()}>
-                          <IoIosLogOut />
-                        </Link>
+                        {user && user.auth === true ?
+                          <Link className="logout-link" onClick={() => handleLogout()}>
+                            <IoIosLogOut />
+                            <span className="logout-text">Đăng xuất</span>
+                          </Link> :
+                          <Link to="/login">
+                            <BiUser /> Đăng nhập
+                          </Link>}
                       </li>
                     </ul>
                   </div>
